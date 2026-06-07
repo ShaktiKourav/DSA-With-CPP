8677416bc84cac172f1091a0ef920f00145bfc11
Shakti Kourav <YOUR_NOREPLY_EMAIL>
2026-06-07 20:37:21 +0530

    Nto1

--- Recursion/printnto1.cpp
+++ Recursion/printnto1.cpp
@@ -6,6 +6,16 @@ void printNto1(int n){
         cout<<1<<" ";
         return;
     }
+
     cout<<n<<" ";
     printNto1(n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+
+    // function call
+    printNto1(n); 
+    return 0;
 }
