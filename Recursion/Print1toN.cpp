f1cecca37d69f9884eee3f468fbe12ee14dcab91
Shakti Kourav <YOUR_NOREPLY_EMAIL>
2026-06-07 20:30:07 +0530

    print1toN

---
+++ Recursion/Print1toN.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// recursive call
+void print1toN(int n) {
+    if(n==1) {
+        cout << 1 << " "; // base case
+        return ; 
+    } else {
+         
+         print1toN(n-1); // recursive call
+         cout << n << " ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number : ";
+    cin >> n;
+  print1toN(n);
+    return 0;
+}
