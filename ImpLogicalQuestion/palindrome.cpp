c1113b70585f6eadccfdc501a4f1a79fa52be542
Shakti Kourav <kouravshakti72@gmail.com>
2026-06-08 11:01:04 +0530

    palindrome

---
+++ ImpLogicalQuestion/palindrome.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int reverse(int n){
+    int rev=0;
+    while(n>0){
+        int rem = n%10;  // last digit 
+        rev = rev*10 + rem; 
+        n = n/10; // remove last digit
+    }
+    return rev;
+}
+
+int main() {
+    int n;
+    int ans ;
+    cout<<"Enter a number :";
+    cin>>n;
+
+    ans = reverse(n);
+    if(ans == n){
+        cout<<"Number is palindrome";
+    }
+    else{
+        cout<<"Number is not palindrome";
+    }
+   
+    return 0;
+}
