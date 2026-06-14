f22c001870a34bee7a75742533989c8a0b9cd2a9
Shakti Kourav <kouravshakti72@gmail.com>
2026-06-14 23:27:32 +0530

    fibonacci series

---
+++ ImpLogicalQuestion/fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
